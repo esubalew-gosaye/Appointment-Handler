2c23f81baea71e9b450ab48a9d304577fe8c0cbe
esubalew-gosaye <mesaye2010@gmail.com>
2023-04-01 15:35:34 +0300

    models creates.

--- appointment/models.py
+++ appointment/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class Schedule(models.Model):
+    doctor = models.DateField()
+    time = models.TimeField()
+
+    def __str__(self):
+        return self.doctor.name + ' ' + self.date + ' ' + self.time
+    
+class Doctor(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200, required=True)
+    phone = models.CharField(max_length=200)
+    speciality = models.CharField(max_length=200)
+    schedule = models.ManyToOneRel(Schedule, blank=True)
+
+    def __str__(self):
+        return self.name
+    
