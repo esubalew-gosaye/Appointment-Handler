9f0f33772c7ed0758e98f2131aa01a8e4562e892
esubalew-gosaye <mesaye2010@gmail.com>
2023-04-02 09:44:43 +0300

    models modified based on form.

--- appointment/models.py
+++ appointment/models.py
@@ -2,20 +2,23 @@ from django.db import models
 
 # Create your models here.
 
-class Schedule(models.Model):
-    sch = models.CharField(max_length=1200, blank=True, null=True)
-    date = models.DateField(blank=True, null=True)
-
-    def __str__(self):
-        return self.sch
-    
 class Doctor(models.Model):
+    class Meta:
+        verbose_name_plural = "Doctors"
+        unique_together = ('email', 'phone')
+
     name = models.CharField(max_length=200, blank=True, null=True)
     email = models.EmailField(max_length=200)
     phone = models.CharField(max_length=200, blank=True, null=True)
     speciality = models.CharField(max_length=200, blank=True, null=True)
-    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.name
+        return self.email
     
+class Schedule(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE, blank=True, null=True)
+    schedule = models.CharField(max_length=200, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.sch
--- appointment/views.py
+++ appointment/views.py
@@ -10,10 +10,12 @@ def date_spliter(date_list, spliter=60):
         st, end, t = elm.split(':')
         for rng in range(int(st), int(end)):
             if t == 'n' or t == 'N':
-                night_schedule.append(rng)
+                if not rng in night_schedule:
+                    night_schedule.append(rng)
             else:
-                day_schedule.append(rng)
-    return night_schedule, day_schedule
+                if not rng in day_schedule:
+                    day_schedule.append(rng)
+    return day_schedule, night_schedule
 def index(request):
     return render(request, 'appointment/index.html', {})
 
@@ -24,7 +26,15 @@ def fill_date(request):
             first_date = request.POST.get('date-first')
             last_date = request.POST.get('date-final')
             list_date = request.POST.get('date-input')
+            
             rs = date_spliter(list_date)
-            print(rs, list_date, first_date, last_date, email)
+
+            # temp_doc = Doctor.objects.create(email=email)
+            
+            if last_date == "":
+                sch = Schedule.objects.create(day_schedule=rs[0], night_schedule=rs[1], date=first_date)
+            else:
+                print('else', rs)
+            print(first_date, last_date, list_date)
 
     return render(request, 'appointment/fill_date_page.html', {})
