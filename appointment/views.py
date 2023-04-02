c7ff776bae402dcf7167dd7b04331bab46481b74
esubalew-gosaye <mesaye2010@gmail.com>
2023-04-02 10:29:10 +0300

    fill_date function built

--- appointment/views.py
+++ appointment/views.py
@@ -16,6 +16,21 @@ def date_spliter(date_list, spliter=60):
                 if not rng in day_schedule:
                     day_schedule.append(rng)
     return day_schedule, night_schedule
+
+def add_schedule(due_date, time, doctor):
+    sch = "Day"
+    for dt in time[0]+["."]+time[1]:
+        if dt == ".":
+            sch = "Night"
+            continue
+        # sch = Schedule.objects.create(
+        #     doctor=temp_doc,
+        #     schedule=f"{dt}:00-{dt+1}:00 {sch}", 
+        #     date=due_date
+        # )
+        print(due_date, f"{dt}:00-{dt+1}:00 {sch}")
+
+
 def index(request):
     return render(request, 'appointment/index.html', {})
 
@@ -26,15 +41,17 @@ def fill_date(request):
             first_date = request.POST.get('date-first')
             last_date = request.POST.get('date-final')
             list_date = request.POST.get('date-input')
-            
+
             rs = date_spliter(list_date)
-
-            # temp_doc = Doctor.objects.create(email=email)
+            y, m, d = first_date.split('-')
             
+            # temp_doc = Doctor.objects.create(email=email)
+            temp_doc = ""
             if last_date == "":
-                sch = Schedule.objects.create(day_schedule=rs[0], night_schedule=rs[1], date=first_date)
+                add_schedule(f"{d}-{m}-{y}", rs, temp_doc)
             else:
-                print('else', rs)
-            print(first_date, last_date, list_date)
-
+                y2, m2, d2 = last_date.split('-')
+                for rdt in range(int(d), int(d2)+1):
+                    add_schedule(f"{rdt}-{m}-{y}", rs, temp_doc)
+                    
     return render(request, 'appointment/fill_date_page.html', {})
